9393208c8a6bf7c2d234f527787c863ab6a5f89a
Garithos2ez4 <luigui2ez4me@gmail.com>
2026-05-05 10:47:01 -0500

    dashboard datos correctos en los apartados nuevos y entregados

--- app/Repositories/RegistroProductoRepository.php
+++ app/Repositories/RegistroProductoRepository.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Repositories;
 
 use Illuminate\Support\Facades\DB;
@@ -13,75 +14,87 @@ class RegistroProductoRepository implements RegistroProductoRepositoryInterface
         // Define las columnas válidas
         $this->modelColumns = (new RegistroProducto())->getFillable();
     }
-    
+
     public function getOne($column, $data)
     {
         $this->validateColumns($column);
-        return RegistroProducto::where($column,'=', $data)->first();
+        return RegistroProducto::query()->where($column, '=', $data, 'and')->first();
     }
 
-    public function getAllByColumn($column,$data)
+    public function getAllByColumn($column, $data)
     {
         $this->validateColumns($column);
-        return RegistroProducto::where($column,'=', $data)->get();
+        return RegistroProducto::query()->where($column, '=', $data, 'and')->get();
     }
 
-    public function paginateAllByColumn($column,$data,$cant)
+    public function paginateAllByColumn($column, $data, $cant)
     {
         $this->validateColumns($column);
-        return RegistroProducto::where($column,'=', $data)->orderBy('fechaMovimiento','desc')->paginate($cant);
+        return RegistroProducto::query()->where($column, '=', $data, 'and')
+            ->orderBy('fechaMovimiento', 'desc')->paginate($cant);
     }
 
-    public function getAllByColumnByThisMonth($column,$data,$cant){
+    public function getAllByColumnByThisMonth($column, $data, $cant)
+    {
         $this->validateColumns($column);
-        return RegistroProducto::where($column,'=',$data)->whereMonth('fechaMovimiento','=',now()->month)->orderBy('fechaMovimiento','desc')->paginate($cant);
+        return RegistroProducto::query()->where($column, '=', $data, 'and')
+            ->whereYear('fechaMovimiento', '=', now()->year, 'and')
+            ->whereMonth('fechaMovimiento', '=', now()->month, 'and')
+            ->orderBy('fechaMovimiento', 'desc')->paginate($cant);
     }
 
     public function searchOne($column, $data)
     {
         $this->validateColumns($column);
-        return RegistroProducto::where($column, 'LIKE', '%' . $data . '%')->first();
+        return RegistroProducto::query()->where($column, 'LIKE', '%' . $data . '%', 'and')->first();
     }
 
     public function searchList($column, $data)
     {
         $this->validateColumns($column);
-        return RegistroProducto::where($column, 'LIKE', '%' . $data . '%')->get();
-    }
-    
-    public function getByIngreso($month){
-        return RegistroProducto::join('IngresoProducto','RegistroProducto.idRegistro','=','IngresoProducto.idRegistro')
-                                    ->select('RegistroProducto.*')->whereMonth('IngresoProducto.fechaIngreso', $month)
-                                    ->orderBy('IngresoProducto.fechaIngreso','desc')
-                                    ->get();
+        return RegistroProducto::query()->where($column, 'LIKE', '%' . $data . '%', 'and')->get();
     }
 
-    public function searchByEgreso($serial,$cant){
-        return RegistroProducto::where('estado','!=','ENTREGADO')
-                                ->where('estado','!=','INVALIDO')
-                                ->where('numeroSerie', 'LIKE', "%{$serial}%")
-                                ->take($cant)
-                                ->get();
+    public function getByIngreso($month)
+    {
+        return RegistroProducto::query()->join('IngresoProducto', 'RegistroProducto.idRegistro', '=', 'IngresoProducto.idRegistro', 'inner', false)
+            ->select('RegistroProducto.*')
+            ->whereYear('IngresoProducto.fechaIngreso', '=', now()->year, 'and')
+            ->whereMonth('IngresoProducto.fechaIngreso', '=', $month, 'and')
+            ->orderBy('IngresoProducto.fechaIngreso', 'desc')
+            ->get();
     }
 
-    public function getByEgreso($serial){
-        return RegistroProducto::where('estado','!=','ENTREGADO')
-                                ->where('estado','!=','INVALIDO')
-                                ->where('numeroSerie','=',$serial)
-                                ->first();
+    public function searchByEgreso($serial, $cant)
+    {
+        return RegistroProducto::where('estado', '!=', 'ENTREGADO', 'and')
+            ->where('estado', '!=', 'INVALIDO', 'and')
+            ->where('numeroSerie', 'LIKE', "%{$serial}%")
+            ->take($cant)
+            ->get();
     }
 
-    public function validateSerie($idProveedor,$serie){
-        $response = RegistroProducto::join('DetalleComprobante','RegistroProducto.idDetalleComprobante','=','DetalleComprobante.idDetalleComprobante')
-                                    ->join('Comprobante','DetalleComprobante.idComprobante','=','Comprobante.idComprobante')
-                                    ->where('RegistroProducto.estado','<>','INVALIDO')
-                                    ->where('Comprobante.idProveedor','=',$idProveedor)
-                                    ->where('RegistroProducto.numeroSerie','=',$serie)->first();
+    public function getByEgreso($serial)
+    {
+        return RegistroProducto::where('estado', '!=', 'ENTREGADO', 'and')
+            ->where('estado', '!=', 'INVALIDO', 'and')
+            ->where('numeroSerie', '=', $serial, 'and')
+            ->first();
+    }
+
+    public function validateSerie($idProveedor, $serie)
+    {
+        $response = RegistroProducto::join('DetalleComprobante', 'RegistroProducto.idDetalleComprobante', '=', 'DetalleComprobante.idDetalleComprobante', 'inner', false)
+            ->join('Comprobante', 'DetalleComprobante.idComprobante', '=', 'Comprobante.idComprobante')
+            ->where('RegistroProducto.estado', '<>', 'INVALIDO', 'and')
+            ->where('Comprobante.idProveedor', '=', $idProveedor, 'and')
+            ->where('RegistroProducto.numeroSerie', '=', $serie)->first();
         return $response;
     }
 
-    public function getSerialsByProduct($idProduct, $idAlmacen = null) {
-        return RegistroProducto::join('DetalleComprobante', 'RegistroProducto.idDetalleComprobante', '=', 'DetalleComprobante.idDetalleComprobante')
+    public function getSerialsByProduct($idProduct, $idAlmacen = null)
+    {
+        return RegistroProducto::join('DetalleComprobante', 'RegistroProducto.idDetalleComprobante', '=', 'DetalleComprobante.idDetalleComprobante', 'inner', false)
             ->join('Producto', 'DetalleComprobante.idProducto', '=', 'Producto.idProducto')
             ->where('RegistroProducto.estado', '<>', 'INVALIDO')
             ->where('RegistroProducto.estado', '<>', 'ENTREGADO')
@@ -92,24 +105,26 @@ class RegistroProductoRepository implements RegistroProductoRepositoryInterface
             ->where('Producto.idProducto', $idProduct)
             ->get();
     }
-    
+
     public function create(array $data)
     {
         return RegistroProducto::create($data);
     }
-    
+
     public function update($id, array $data)
     {
         $reg = RegistroProducto::findOrFail($id);
         $reg->update($data);
         return $reg;
     }
-    
-    public function getLast(){
+
+    public function getLast()
+    {
         return RegistroProducto::orderBy('idRegistro', 'desc')->first();
     }
-    
-    private function validateColumns($column){
+
+    private function validateColumns($column)
+    {
         if (!in_array($column, $this->modelColumns)) {
             throw new \InvalidArgumentException("La columna '$column' no es válida.");
         }
